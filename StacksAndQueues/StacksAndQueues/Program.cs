99d3119304e9e2e811bb112920277cced32ad3d3
Nepolean23 <nlean1996@gmail.com>
2022-12-09 20:06:01 +0530

    Performs a Peak,Pop and Isempty In LinkedList

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,7 +1,12 @@
 ﻿using StacksAndQueues;
 
-UC_1_AbilityToCreateAStack list= new UC_1_AbilityToCreateAStack();
-list.Push(56);
-list.Push(30);
-list.Push(70);
-list.display();
+//UC_1_AbilityToCreateAStack list= new UC_1_AbilityToCreateAStack();
+//list.Push(56);
+//list.Push(30);
+//list.Push(70);
+//list.display();
+
+UC_2_AbilityToPeakAndPopOperation operation=new UC_2_AbilityToPeakAndPopOperation();
+operation.peek();
+operation.pop();
+operation.IsEmpty();
---
+++ StacksAndQueues/StacksAndQueues/UC_2_AbilityToPeakAndPopOperation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class UC_2_AbilityToPeakAndPopOperation
+    {
+        private Node Top;
+        internal void peek()
+        {
+            if(this.Top==null)
+            {
+                Console.WriteLine("The Stack is Empty");
+                return;
+
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.Top.next);
+        }
+
+        internal void pop() 
+        {
+            if(Top==null)
+            {
+                Console.WriteLine("Stack is empty,Deletion is not possible");
+                return;
+
+            }
+            Console.WriteLine("Value popped is {0}", this.Top.data);
+            this.Top=this.Top.next
+        }
+        
+        internal void IsEmpty()
+        {
+            while(this.Top!=null) 
+            {
+                peek();
+                pop();
+            }
+        }
+    }
+}
