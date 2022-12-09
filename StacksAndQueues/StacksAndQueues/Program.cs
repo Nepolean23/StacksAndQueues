ee7f5a2a091c27d2a66cd5efe75753c914bd9139
Nepolean23 <nlean1996@gmail.com>
2022-12-09 19:43:03 +0530

    Stacking Of LinkedList

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StacksAndQueues;
+
+UC_1_AbilityToCreateAStack list= new UC_1_AbilityToCreateAStack();
+list.Push(56);
+list.Push(30);
+list.Push(70);
+list.display();
---
+++ StacksAndQueues/StacksAndQueues/UC_1-AbilityToCreateAStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class UC_1_AbilityToCreateAStack
+    {
+        private Node Top;
+
+        public  UC_1_AbilityToCreateAStack()
+        {
+            this.Top = null;
+        }
+        
+        internal void Push(int value)
+        {
+            Node Node = new Node(value);
+            if (this.Top == null)
+                Node.next = null;
+            else
+                Node.next = this.Top;
+                this.Top=Node;
+            Console.WriteLine("{0} pushed to stack" +value);
+        }
+        internal void display()
+        {
+            Node temp = this.Top;
+            while(temp!= null ) 
+            {
+                Console.WriteLine(temp.data+ "");
+                temp = temp.next;
+            }
+        }
+        
+            
+        
+    }
+}
